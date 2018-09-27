344c8883c8e3e134f149f93a9953a681e371b9b7
Sam Dalton <spd4nf@virginia.edu>
2018-09-27 14:05:33 -0500

    added add item and check item

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+/*eslint-env jquery */
+
+function updateShoppingList(){
+    //add item
+    $('#js-shopping-list-form').submit(event => {
+        event.preventDefault();
+        const item = ($(this).find('.js-shopping-list-entry').val());
+        $('.shopping-list').append(generateNewItemHTML(item));
+    });
+    //check item
+    $('.shopping-list').on('click','.shopping-item-toggle',( function(event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    }));
+    //remove item
+    $('.shopping-list').on('click', 'shopping-item-delete', (function (event){
+        $(this).clost('li').
+    }));
+}
+
+function generateNewItemHTML(item){
+    return `<li>
+        <span class="shopping-item">${item}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`;
+}
+
+$(updateShoppingList);
